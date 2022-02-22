c1945d8e60165c998aa5ed7878b610edaf13e20e
Amit Yadav <amityadav@cedcoss.com>
2022-02-22 18:36:08 +0530

    prime checker Jquery

--- src/script.js
+++ src/script.js
@@ -1,17 +1,15 @@
-document.getElementById("primeChecker").onclick =function display(){
-    var pr=parseInt(document.getElementById("number").value);
+$("#primeChecker").click(function(){
+    var pr=parseInt($("#number").val());
     var flag = true;
     for (var i=2;i<pr;i++) {
         if (pr % i == 0) {
-            console.log(typeof(pr));
             flag = false;
             break;
         }
     }
     if (flag) {
-        console.log(typeof(pr));
-        document.getElementById("prime").innerHTML= pr+" Is a prime number";
+        $("#prime").html(pr+" Is a prime number");
     } else {
-        document.getElementById("prime").innerHTML= pr+" Is not a prime number";
+        $("#prime").html(pr+" Is not a prime number");
     }
-}
+});
