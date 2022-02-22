c98a7001535c889fbe4fb16c241020b64f17352c
Amit Yadav <amityadav@cedcoss.com>
2022-02-22 18:55:28 +0530

    reaction tester -Javascript

--- src/script.js
+++ src/script.js
@@ -1,15 +1,41 @@
-$("#primeChecker").click(function(){
-    var pr=parseInt($("#number").val());
-    var flag = true;
-    for (var i=2;i<pr;i++) {
-        if (pr % i == 0) {
-            flag = false;
-            break;
-        }
+var start = new Date().getTime();
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
     }
-    if (flag) {
-        $("#prime").html(pr+" Is a prime number");
+    return color;
+}
+        
+function appear() {
+    var top = Math.random() * 400;
+    var left = Math.random() * 400;
+    var width = (Math.random() * 200) + 100;
+    if (Math.random() > 0.5) {
+        document.getElementById("shape").style.borderRadius = "50%";
     } else {
-        $("#prime").html(pr+" Is not a prime number");
+        document.getElementById("shape").style.borderRadius = "0";
     }
-});
+    document.getElementById("shape").style.backgroundColor = getRandomColor();
+    document.getElementById("shape").style.width = width + "px";
+    document.getElementById("shape").style.height = width + "px";
+    document.getElementById("shape").style.top = top + "px";
+    document.getElementById("shape").style.left = left + "px";
+    document.getElementById("shape").style.display = "block";
+    start = new Date().getTime();
+}
+
+function appearAfterDelay() {
+    setTimeout(appear, Math.random() * 2000);
+}
+        
+appearAfterDelay();
+document.getElementById("shape").onclick=function() {    
+    document.getElementById("shape").style.display="none";
+    var end=new Date().getTime();
+    var timeTaken=(end-start)/1000;
+    document.getElementById("timeTaken").innerHTML=timeTaken+"s";
+    appearAfterDelay();
+}
